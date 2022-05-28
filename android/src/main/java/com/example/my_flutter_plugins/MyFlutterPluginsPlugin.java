8b6a3f796f6d5560497d83a8df4a12aca8e28f0c
Sukhdeep Singh <sukhdeepmankoo@gmail.com>
2022-05-28 23:17:00 +0530

    renamed

--- android/src/main/java/com/example/my_flutter_plugins/MyFlutterPluginsPlugin.java
+++ android/src/main/java/com/example/my_flutter_plugins/MyFlutterPluginsPlugin.java
@@ -25,7 +25,7 @@ public class MyFlutterPluginsPlugin implements FlutterPlugin, MethodCallHandler 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
-      result.success("Custom Android " + android.os.Build.VERSION.RELEASE);
+      result.success("Platform Android " + android.os.Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
     }
